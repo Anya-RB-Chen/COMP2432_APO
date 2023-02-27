9491b39eca6f1be26f1e08b7df93b86a0a63fe58
MikeYuanP <124416029+MikeYuanP@users.noreply.github.com>
2023-02-27 11:06:45 +0800

    hhh
    hhh

---
+++ COMP2432_project_G02/modules/output_module.c
@@ -0,0 +1,16 @@
+//
+// Created by 袁蕴宸 on 25/2/2023.
+//
+
+#include "output_module.h"
+
+
+#include "../classes/scheduling.h"
+#include "../main.h"
+
+
+//output module:
+
+void outputModule (int** scheduleMatrix, enum SCHEDULING_ALGORITHM algorithm) {
+
+}
