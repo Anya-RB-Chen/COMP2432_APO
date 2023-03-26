5c675904dc3bac4b1dbfaf076a9859058ac17029
zhangwxleo <92870596+zhangwxleo@users.noreply.github.com>
2023-03-26 22:56:01 +0800

    protocol部分

---
+++ COMP2432_project_G02/protocol/appointment_notification_protocol.c
@@ -0,0 +1,242 @@
+//
+// Created by 张文轩 on 2023/3/26.
+//
+#include "protocol.h"
+#include "appointment_notification_protocol.h"
+#include "coding_tools.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char* Int2String(int num,char *str)
+{
+    int i = 0;
+    do
+    {
+        str[i++] = num%10+48;
+        num /= 10;
+    }while(num);
+    str[i] = '\0';
+    int j = 0;
+    for(;j<i/2;j++)
+    {
+        str[j] = str[j] + str[i-1-j];
+        str[i-1-j] = str[j] - str[i-1-j];
+        str[j] = str[j] - str[i-1-j];
+    }
+    return str;
+}
+
+//level 2: presentation layer interface
+char *appointmentNotification_protocol_requestMessage_encoding(SAppointment ap) {
+    // Assumption: each field of appointment won't be null. If type is privateTime which only includes caller,
+    // we have to assign callee a value. eg: "0";
+    int i;
+    static char res[100] = "|";
+    char buffer[10];
+    char *seperate = "|";
+    char *sepCallee = "&";
+    char *none = "@";
+    char *type = Int2String(ap.type,buffer);
+    strcat(res,type);
+    strcat(res,seperate);
+    char *year = Int2String(ap.startTime.year,buffer);
+    strcat(res,year);
+    strcat(res,seperate);
+    char *month = Int2String(ap.startTime.month,buffer);
+    strcat(res,month);
+    strcat(res,seperate);
+    char *date = Int2String(ap.startTime.day,buffer);
+    strcat(res,date);
+    strcat(res,seperate);
+    char *hour = Int2String(ap.startTime.hour,buffer);
+    strcat(res,hour);
+    strcat(res,seperate);
+    char *minute = Int2String(ap.startTime.minute,buffer);
+    strcat(res,minute);
+    strcat(res,seperate);
+    char dura[10];
+    sprintf(dura,"%.1f",ap.duration);
+    strcat(res,dura);
+    strcat(res,seperate);
+    char *caller = ap.caller;
+    strcat(res,caller);
+    strcat(res,seperate);
+    char *idx = Int2String(ap.apIndex,buffer);
+    strcat(res,idx);
+    strcat(res,seperate);
+    for(i = 0; i<ap.numberOfCallee; i++){
+        strcat(res,ap.callee[i]);
+        strcat(res,sepCallee);
+    }
+    strcat(res,seperate);
+    char *numCallee = Int2String(ap.numberOfCallee,buffer);
+    strcat(res,numCallee);
+    strcat(res,seperate);
+    return res;
+}
+
+
+SAppointment *appointmentNotification_protocol_requestMessage_decoding(char *message) {
+    SAppointment *a = (SAppointment*) malloc(sizeof (SAppointment));
+    int i = 1, j = 0, k;
+    a->type = message[i] - 48;
+    i += 2;
+    char year[10];
+    while (message[i] != '|'){
+        year[j] = message[i];
+        i++;
+        j++;
+    }
+    j = 0;
+    i++;
+    a->startTime.year = atoi(year);
+    printf("%d\n",a->startTime.year);
+
+    // get month
+    char month[2];
+    while (message[i] != '|'){
+        month[j] = message[i];
+        i++;
+        j++;
+    }
+    char res[j];
+    for (k = 0; k<j; k++) res[k] = month[k];
+    j = 0;
+    i++;
+    a->startTime.month = atoi(res);
+
+
+    // get date
+    char day[2];
+    while (message[i] != '|'){
+        day[j] = message[i];
+        i++;
+        j++;
+    }
+    char res1[j];
+    for (k = 0; k<j; k++) res1[k] = day[k];
+    j = 0;
+    i++;
+    a->startTime.day = atoi(res1);
+
+    // get start hour
+    char hour[2];
+    while (message[i] != '|'){
+        hour[j] = message[i];
+        i++;
+        j++;
+    }
+    char res2[j];
+    for (k = 0; k<j; k++) res2[k] = hour[k];
+    j = 0;
+    i++;
+    a->startTime.hour = atoi(res2);
+
+    // get start minute
+    char minute[2];
+    while (message[i] != '|'){
+        minute[j] = message[i];
+        i++;
+        j++;
+    }
+    char res3[j];
+    for (k = 0; k<j; k++) res3[k] = minute[k];
+    j = 0;
+    i++;
+    a->startTime.minute = atoi(res3);
+
+
+    // get duration
+    char dura[10];
+    while (message[i] != '|'){
+        dura[j] = message[i];
+        i++;
+        j++;
+    }
+    j = 0;
+    i++;
+    a->duration = atof(dura);
+
+
+    // get caller
+    char caller[10];
+    int m;
+    for (m = 0; m<10; m++){
+        caller[m] = '\0';
+    }
+    while (message[i] != '|'){
+        caller[j] = message[i];
+        i++;
+        j++;
+    }
+    printf("%s\n",caller);
+    strcpy(a->caller,caller);
+    printf("%s\n",a->caller);
+    j = 0;
+    i++;
+
+
+    // get appointment index
+    char idx[3];
+    while (message[i] != '|'){
+        idx[j] = message[i];
+        i++;
+        j++;
+    }
+    j = 0;
+    i++;
+    a->apIndex = atoi(idx);
+
+    char temp[10];
+    for (m = 0; m<10; m++){
+        temp[m] = '\0';
+    }
+    while (message[i] != '|'){
+        k = 0;
+        while (message[i] != '&'){
+            temp[k] = message[i];
+            i++;
+            k++;
+        }
+        i++;
+        strcpy(a->callee[j],temp);
+        printf("%s\n",a->callee[j]);
+        for (m = k-1; m<10; m++){
+            temp[m] = '\0';
+        }
+        j++;
+    }
+    a->numberOfCallee = j;
+
+    return a;
+}
+
+SAppointment appointmentNotification_protocol_interpret_request (int rp){
+    SAppointment appointment;
+    int n;
+    char buffer[100];
+    n = read(rp,buffer,100); // 不带port number，正常decode
+    // this message doesn't contain port number. Decode by normal procedure
+    appointment = *appointmentNotification_protocol_requestMessage_decoding(buffer);
+    return appointment;
+}
+
+void  appointmentNotification_protocol_API_(SAppointment ap,  int wp) {
+    // the appointment should be encoded with the port number
+    // new format: |port_number|appointment_information
+    char res[100];
+    char *seperate = "|";
+    char* msg = appointmentNotification_protocol_requestMessage_encoding(ap);
+    char port_buffer[4];
+    integer_little_endian_encoding(APPOINTMENT_NOTIFICATION_PROTOCOL_PORT_NUMBER,port_buffer);
+    strcpy(res,port_buffer);
+    strcat(res,seperate);
+    strcat(res,msg);
+
+    write(wp,msg,sizeof (msg));
+}
+
--- COMP2432_project_G02/protocol/protocol.h
+++ COMP2432_project_G02/protocol/protocol.h
@@ -15,11 +15,12 @@
 extern const int APPOINTMENT_NOTIFICATION_PROTOCOL_PORT_NUMBER;
 
 void  appointmentNotification_protocol_API_(SAppointment ap,  int wp);
+
 //新加的接口：
 //输入： read pointer (read pointer 指向的pipe 队列头为request message （不含port number) ）
-//输出： Scard
+//输出： SAppointment
 //process : read,  decoding, return.
-SCard appointmentNotification_protocol_interpret_request (int rp)
+SAppointment appointmentNotification_protocol_interpret_request (int rp);
 
 
 //2, schudule requring protocol
@@ -38,8 +39,9 @@ SCHEDULING_ALGORITHM scheduleRequering_protocol_interpret_request (int rp);
 
 //input: schedule map (length * 2 array)  ， write pointer.
 //output: 通过wp， 把schedule map 写给parent。  和下面的 scheduleRequering_protocol_recipientAPI 统一起来
-void scheduleRequering_protocol_deliverScheduleMap (int scheduleMap[][2], int length, int wp);
-
+void scheduleRequering_protocol_deliverScheduleMap (int **scheduleMap, int length, int wp);
+//schedule map encode完了之后把message写给parent
+//
 
 //recipient API
 //------------------------------------------------------------------------------------------------------------------------//--------------------------------------------------------------------------------------------------------------------------
@@ -49,9 +51,18 @@ void scheduleRequering_protocol_deliverScheduleMap (int scheduleMap[][2], int le
 
 //new:
 //return value: length of array,   + write the content of array to personalSchedule
-int  scheduleRequering_protocol_recipientAPI(SCHEDULING_ALGORITHM algorithm, int rp, int wp,int (*personalSchedule)[2]);
+int  scheduleRequering_protocol_recipientAPI(SCHEDULING_ALGORITHM algorithm, int rp, int wp,int **personalSchedule);
 //--------------------------------------------------------------------------------------------------------------------------
+// 先把algorithm写给child，等child传回来schedule信息之后decode，二维数组写给指针，长度作为返回值返回
+// 保证上层函数能访问到数组长度（schedule的数量）
 
+// 上层函数对schedule map的定义：
+// let k be a large number
+//int ** test = (int **) malloc(sizeof(int **) * 2);
+//test[0] = (int *) malloc(sizeof (int *)*k);
+//test[1] = (int *) malloc(sizeof (int *)*k);
+
+// free (test)
 
 
 #endif //COMP2432_APO_PROTOCOL_H
