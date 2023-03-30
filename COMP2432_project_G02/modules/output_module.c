28b508e2818b74074893fad3f42bfb0e4fde4c1f
Anya-R-Chen <108870151+Anya-RB-Chen@users.noreply.github.com>
2023-03-30 19:57:57 +0800

    Update output_module.c

--- COMP2432_project_G02/modules/output_module.c
+++ COMP2432_project_G02/modules/output_module.c
@@ -2,10 +2,15 @@
 // Created by 袁蕴宸 on 25/2/2023.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-
-#include "../classes/scheduling.h"
-#include "../main.h"
+#include "../main.h"                //include the main function
+#include "../classes/scheduling.c" 
+#include "../classes/appointment.c"
+#include "../classes/time_type.c"             
 
 
 //output module:
@@ -15,5 +20,25 @@
 //need to maintain the sequence of output file.
 void outputModule (int rows, int columns, int scheduleMatrix[][columns], SCHEDULING_ALGORITHM algorithm) {
 
+    if (g_startTime.day == NULL || g_endTime.day == NULL) {
+        g_startTime = getTime("20230401", "0000");
+        g_endTime = getTime("20230430", "2359");
+    }
+    
+    printf("Period 2023-04-01 to 2023-04-30\n");
+
+    char* schedule_name = get_SchedingAlgorithm_name(algorithm);
+    if (strcmp(schedule_name, "") == 0) {
+        strcpy(schedule_name, "ALL");
+    }
+    printf("Algorithm used: %s\n", schedule_name);
+
+    printf("\n***Appointment Schedule***\n");
 }
 //--------------------------------------------------------------------------------------------------------------------------
+
+// Only for test usage
+void main() {
+    int scheduleMatrix[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    outputModule(3, 3, scheduleMatrix, FCFS);
+}
